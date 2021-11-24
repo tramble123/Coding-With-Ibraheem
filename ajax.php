2649629a4271dc03dd39a09a2d6ba95cc6f779c1
tramble123 <94967323+tramble123@users.noreply.github.com>
2021-11-24 16:59:07 +0530

    Create ajax.php

---
+++ ajax.php
@@ -0,0 +1,17 @@
+	<?php
+$action = $_GET['action'];
+
+if($action == 'login-request'){
+extract($_POST);		
+		$qry = $this->db->query("SELECT * FROM users where username = '".$username."' and password = '".$password."' ");
+		if($qry->num_rows > 0){
+			foreach ($qry->fetch_array() as $key => $value) {
+				if($key != 'passwors' && !is_numeric($key))
+					$_SESSION['login_'.$key] = $value;
+			}
+				return 1;
+		}else{
+			return 3;
+		}
+}
+?>
